7ae86e53a6e643e23a117032ed41d43cd2b1d16d
Jesa Nina C. Lacambra <jlacambra@blaine.local>
2021-01-07 12:10:12 +0800

    add header

--- application/views/inc/navbar.php
+++ application/views/inc/navbar.php
@@ -531,6 +531,23 @@
         });
     </script>
     <body>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm stroke" style="background-color: #003153 !important; height: 65px">
+        <div class="container">
+            <a class="navbar" href="">
+                
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                </ul>
+                <ul class="navbar-nav ml-auto fw">
+                    <!--user name-->
+                </ul>
+            </div>
+        </div>
+    </nav>
         <div class="page-wrapper chiller-theme toggled">
             <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
                 <i class="fas fa-bars"></i>
