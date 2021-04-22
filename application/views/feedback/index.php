e7ba01466a177fc686d73be46f82fd1d40a20266
blaine <blaine.com>
2021-04-22 09:35:07 +0800

    old update

--- application/views/feedback/index.php
+++ application/views/feedback/index.php
@@ -4,13 +4,8 @@
 <?php if($this->session->flashdata('error_msg')) : ?>
     <p class="alert alert-dismissable alert-danger"><?php echo $this->session->flashdata('error_msg'); ?></p>
 <?php endif; ?>
-<<<<<<< HEAD
-<div class="card-header" style="background-color:#1C4670; color:white;"><h4>Blaine Feedback List
-<?php if($this->session->userdata('access_level_id') == 1 || $this->session->userdata('access_level_id') == 3) : ?><a href="#" class="btn btn-dark float-right"  data-toggle="modal" data-target="#exampleModal" style="border:1px solid #ccc; margin-right:10px;">ADD</a> <?php endif; ?>
-=======
 <div class="card-header" style="background-color:#1C4670; color:white;"><h4>Blaine E-Feedback List
 <?php if($this->session->userdata('access_level_id') == 1) : ?><a href="#" class="btn btn-dark float-right"  data-toggle="modal" data-target="#exampleModal" title="Add Feedback" style="border:1px solid #ccc; margin-right:10px;">ADD</a> <?php endif; ?>
->>>>>>> master
     </h4> 
 </div>
 <br> 
